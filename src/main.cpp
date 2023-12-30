ef4bd80d3f1fa675f4e0c20854fb4c8b82b58b69
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2023-12-30 16:12:07 +0700

    chore: list todos

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #include "Server.h"
 #include "SocketManager.h"
 
+/*
+TODO:
+- fix recv_msg logic
+- tidy up main server logic
+*/
+
 int main(int argc, char **argv) {
   Server server;
   server.run();
