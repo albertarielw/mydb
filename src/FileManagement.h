f1a7f41b1cde35074a1556775ac948577b5bd33f
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2024-01-02 00:27:20 +0700

    feat: add config and file management class

--- src/FileManagement.h
+++ src/FileManagement.h
@@ -2,14 +2,14 @@
 #define FILEMANAGEMENT_H
 
 #include <string>
+#include "Config.h"
 
 class FileManagement {
 public:
-  FileManagement(const std::string& database_directory, const std::string& database_filename)
-    : database_directory(database_directory), database_filename(database_filename) {}
-
-  std::string get_database_directory();
-  std::string get_database_filename();
+  FileManagement(Config & config) {
+    database_directory = config.get_database_directory();
+    database_filename = config.get_database_filename();
+  }
 
 private:
   std::string database_directory, database_filename;
